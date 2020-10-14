002ec33860a36cab120bf3523ea6e2e77b8ac1ce
Nayla Massara <nay_massara03@hotmail.com>
2020-10-14 20:55:21 -0300

    Ajax

--- main.js
+++ main.js
@@ -1,22 +1,35 @@
 //alert('¡Bienvenida a nuestro sitio! Para recibir novedades y promociones no dejes de subscribirte');
 
-function validarDatos() {
+$("#botonsesion").click(function (event) {
+    event.preventDefault()
     var email = document.getElementById("email").value;
     var clave = document.getElementById("clave").value;
 
-    if (email != 0 && clave != 0) {
-        alert("Sesión iniciada")
+    if (email === "margarita54@yahoo.com" && clave === "marga") {
+        welcomeUser(email)
     }
     else {
-        alert("Por favor inicie sesión para acceder a la tienda")
+        alert("El usuario y contreseña no son validos")
     }
 
+})
+
+function welcomeUser(email) {
+    $.ajax({
+        url: "usuario.json", success: function (data) {
+
+            const user = data.filter(usuario => usuario.email === email)
+            alert(`Bienvenida ${user[0].firstname}`)
+        }
+    });
 }
 
-function validarDatos2() {
+function registrar() {
     var email = document.getElementById("email2").value;
     var clave = document.getElementById("clave2").value;
     var confirmarclave = document.getElementById("confirmarclave2").value;
+
+    login(email, clave)
 
     if (email != 0 && clave != 0 && clave === confirmarclave) {
         alert("Su cuenta se ha creado con éxito")
@@ -27,8 +40,8 @@ function validarDatos2() {
     else {
         alert("Por favor asegurese de haber rellenado todos los campos")
     }
-
 }
+
 
 var wishList = localStorage.getItem('wishList') ? JSON.parse(localStorage.getItem('wishList')) : []
 
@@ -36,7 +49,7 @@ wishList.forEach(productName => addToList(productName))
 
 document.querySelectorAll('.target-cartera').forEach(boton => {
     boton.onclick = () => {
-       
+
         var productName = boton.parentNode.parentNode.childNodes[1].textContent
         if (wishList.indexOf(productName) === -1) {
             wishList.push(productName)
@@ -48,78 +61,23 @@ document.querySelectorAll('.target-cartera').forEach(boton => {
         }
     }
 })
- 
- function addToList(productName) {
-     var insertarALista = document.createElement("li");
-     insertarALista.innerHTML = productName;
-     document.querySelector(".lista").appendChild(insertarALista);
- } 
 
- $("nav").hover(function(){
+function addToList(productName) {
+    var insertarALista = document.createElement("li");
+    insertarALista.innerHTML = productName;
+    document.querySelector(".lista").appendChild(insertarALista);
+}
+
+$("nav").hover(function () {
     $(this).css("background-color", "#cb997e");
-    }, function(){
+}, function () {
     $(this).css("background-color", "#ddbea9");
-  });
+});
 
-  $("#hoverLogo").hover(function(){
+$("#hoverLogo").hover(function () {
     $(this).css("width", "50")
     $(this).css("height", "50");
-    }, function(){
+}, function () {
     $(this).css("width", "30")
     $(this).css("height", "30");
-  });
-
-// const cartera3 = document.querySelector("#alejandrita")
-// cartera3.addEventListener("click", () => {
-//     listaDeDeseo.push("carteraAlejandrita")
-//     alert("Agregaste la cartera Alejandrita a tu lista de deseos")
-//     var insertarALista = document.createElement("li");
-//     insertarALista.innerHTML = "Cartera Alejandrita";
-//     document.querySelector(".lista").appendChild(insertarALista);
-// })
-
-
-
-
-
-
-/*
-
-
-function myFunction() {
-    var para = document.createElement("li");
-    para.innerHTML = "cartera";
-    document.querySelector(".lista").appendChild(para);
-  }
-
-
- */
-
-    // function impimirEnLista (carteras){
-    // var nuevo= document.createElement("li") 
-    // nuevo .innerHTML= "carteras"                   
-    // var lista=document.querySelector(".lista")
-    // lista.appenChild(nuevo)
-    // }
-
-    // impimirEnLista("cartera")
-
-    // const content = element.insertAdjacentHTML("nayla")
-/*
-/* listaDeDeseo.forEach(element => {
-    var nuevo= document.createElement("li")
-    nuevo.textContent= element
-    var lista=document.querySelector(".lista")
-    lista.appenChild(nuevo)
-})  */
-/* function agregarALista(element) {
-
-    listaDeDeseo.forEach(element => {
-        var li= document.createElement("li")
-        li.textContent= element
-        var ul=document.querySelector(".lista")
-        ul.appenChild(li)
-    })
-
-    listaDeDeseo.forEach(element => console.log("paso adentro"))
-    } */
+});
